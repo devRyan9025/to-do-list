e631b573576c48adb7d02f1b6865cbc5b7857406
devRyan9025 <alex7006@naver.com>
2024-10-31 08:58:08 +0900

    refactor : Optimize the code using context

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import './App.css';
-import { useRef, useReducer, useCallback } from 'react';
+import {
+  useRef,
+  useReducer,
+  useCallback,
+  createContext,
+  useMemo,
+} from 'react';
 import Header from './components/Header';
 import Editor from './components/Editor';
 import List from './components/List';
@@ -24,6 +30,7 @@ const mockData = [
     date: new Date().getTime(),
   },
 ];
+
 function reducer(state, action) {
   switch (action.type) {
     case 'CREATE':
@@ -40,6 +47,10 @@ function reducer(state, action) {
       return state;
   }
 }
+
+export const TodoStateContext = createContext();
+
+export const TodoDispatchContext = createContext();
 
 function App() {
   const [todos, dispatch] = useReducer(reducer, mockData);
@@ -75,15 +86,19 @@ function App() {
     });
   }, []);
 
+  const memoizedDispatch = useMemo(() => {
+    return { onCreate, onUpdate, onDelete };
+  }, []);
+
   return (
     <div className="App">
       <Header />
-      <Editor onCreate={onCreate} />
-      <List
-        todos={todos}
-        onUpdate={onUpdate}
-        onDelete={onDelete}
-      />
+      <TodoStateContext.Provider value={todos}>
+        <TodoDispatchContext.Provider value={memoizedDispatch}>
+          <Editor />
+          <List />
+        </TodoDispatchContext.Provider>
+      </TodoStateContext.Provider>
     </div>
   );
 }
